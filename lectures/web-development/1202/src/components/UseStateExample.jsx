d502a32b871efa5b11730f73930320d902b62a9f
Leader_Ethan <ethanyun022@gmail.com>
2025-12-02 12:39:36 +0900

    feat: 1202 학습 자료 체계화 및 docs 폴더 분리

---
+++ lectures/web-development/1202/src/components/UseStateExample.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function UseStateExample() {
+  //   let count = 0;
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    // count = count + 1;
+    setCount((pre) => pre + 1);
+    console.log(count);
+  };
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={increase}>증가</button>
+    </div>
+  );
+}
+
+export default UseStateExample;
